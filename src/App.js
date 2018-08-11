db503e65dd912971a11f582cfac922e14d25d738
Biyun Wu <beenwu2016@gmail.com>
2018-08-11 00:55:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import GoogleMap from './components/GoogleMapsContainer'
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <GoogleMap />
+        )
+    }
+}
+
+export default App;
