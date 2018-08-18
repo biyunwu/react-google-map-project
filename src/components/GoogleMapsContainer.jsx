7e0489318a93196297ce5cbb28cd14b93ce7d2c8
Biyun Wu <beenwu2016@gmail.com>
2018-08-18 17:23:10 -0400

    Adjust InfoWindow

--- src/components/GoogleMapsContainer.jsx
+++ src/components/GoogleMapsContainer.jsx
@@ -30,10 +30,11 @@ class GoogleMapsContainer extends React.Component {
             activeMarker: marker,
             showingInfoWindow: true
         })
-        console.log('props, props.id', props, props.id)
+        // console.log('props, props.id', props, props.id)
         fetch(this.getRequestString(props.id))
             .then(this.checkResponse)
-            .then(dt => this.setState({currVenue: dt && dt.response? dt.response.venue : null}))
+            .then(dt => dt && dt.response && dt.response.venue ? dt.response.venue : null)
+            .then(venue => this.setState({currVenue: venue}))
     }
 
     checkResponse = (res) => {
@@ -43,6 +44,7 @@ class GoogleMapsContainer extends React.Component {
             // alert('No response from server!')
             document.getElementById('footertext').innerText = 'Only basic info is available due to the daily request limitation of Foursqaure API.'
             document.getElementById('footer').style.backgroundColor = 'red'
+            return 0
         }
     }
 
@@ -55,22 +57,22 @@ class GoogleMapsContainer extends React.Component {
         }
     }
 
-    getRequestString = (id) => `https://api.foursquare.com/v2/venues/${id}?&client_id=LRYG3OLF2LZTRVK3JFNX22XED5SGGA1P32BIHPG5RYGXMLDO&client_secret=GKJMX3KNN1V5W3SLTB1QPVWQXCNG533GKAXJ1VK0ATWI5SIL&v=20180323&limit=1`
+    getRequestString = (id) => `https://api.foursquare.com/v2/venues/${id}?&client_id=LRYG3OLF2LZTRVK3JFNX22XED5SGGA1P32BIHPG5RYGXMLDO&client_secret=GKJMX3KNN1V5W3SLTB1QPVWQXCNG533GKAXJ1VK0ATWI5SIL&v=20180817&limit=1`
 
     render() {
         const currVenue = this.state.currVenue
         const selectedPlace = this.state.selectedPlace
         const venueInfo = {}
         if (currVenue && selectedPlace && selectedPlace.id === currVenue.id) {
-            venueInfo.url = currVenue.url ? currVenue.url : 0
+            venueInfo.url = currVenue.url ? currVenue.url : undefined
             venueInfo.price = currVenue.attributes.groups[0].summary.includes('$')
-                                ? currVenue.attributes.groups[0].summary : 0
-            venueInfo.rating = currVenue.rating? currVenue.rating : 0
-            venueInfo.phone = currVenue.contact.formattedPhone || 0
-            venueInfo.category = currVenue.categories[0].shortName || 0
+                                ? currVenue.attributes.groups[0].summary : undefined
+            venueInfo.rating = currVenue.rating? currVenue.rating : undefined
+            venueInfo.phone = currVenue.contact.formattedPhone || undefined
+            venueInfo.category = currVenue.categories[0].shortName || undefined
             venueInfo.delivery = currVenue.delivery && currVenue.delivery.url
                                     ? currVenue.delivery.url
-                                    : 0
+                                    : undefined
         }
         console.log('currInfo', venueInfo);
 
@@ -95,7 +97,7 @@ class GoogleMapsContainer extends React.Component {
                     id={r.id}
                     onClick = { this.onMarkerClick }
                     title = { r.name }
-                    address = {r.location.formattedAddress.join(', ')}
+                    address = {r.location.formattedAddress[0]}
                     position = {{ lat: r.location.lat, lng: r.location.lng }}
                     name = { r.name }
                     />
@@ -110,10 +112,10 @@ class GoogleMapsContainer extends React.Component {
                             <b><a href={venueInfo.url}>{this.state.selectedPlace.name}</a></b>
                             <span class='dollarsign'>{venueInfo.price}</span>
                         </p>
-                        <p>{this.state.selectedPlace.address}</p>
                         {venueInfo.category && <p>Style: {venueInfo.category}</p>}
                         {venueInfo.delivery && <p><a href={venueInfo.delivery}>Delivery Available</a></p>}
-                        <p>{venueInfo.phone && {Contact: venueInfo.phone}}</p>
+                        {venueInfo.phone && <p>{venueInfo.phone}</p>}
+                        <p>{this.state.selectedPlace.address}</p>
                     </div>
                 </InfoWindow>
             </Map>
