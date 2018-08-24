497f5a9ca55493d9f9659ab852314f170ba147f9
Biyun Wu <beenwu2016@gmail.com>
2018-08-23 22:40:47 -0400

    Change Footer to functional component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <footer id='footer'>
-                <p id='footertext'>Developed with appetite by <a href='https://biyunwu.com' target="_blank" rel="noopener noreferrer">Biyun Wu</a>.</p>
-            </footer>
-        )
-    }
+// export default class Footer extends Component {
+//     render() {
+//         return (
+//             <footer id='footer'>
+//                 <p id='footertext'>Developed with appetite by <a href='https://biyunwu.com' target="_blank" rel="noopener noreferrer">Biyun Wu</a>.</p>
+//             </footer>
+//         )
+//     }
+// }
+
+export default function Footer () {
+    return(
+        <footer id='footer'>
+            <p id='footertext'>Developed with appetite by <a href='https://biyunwu.com' target="_blank" rel="noopener noreferrer">Biyun Wu</a>.</p>
+        </footer>
+    )
 }
